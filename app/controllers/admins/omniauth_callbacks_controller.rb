1d5c922aed818f16cb2257f053b1752ae9bc57c7
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-03-12 14:57:43 +0530

    change in route

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@
 class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def google_oauth2
+    puts "-----------------------------------------------------------"
     @admin = Admin.find_for_google_oauth2(request.env["omniauth.auth"], current_admin)
     if @admin.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
 # 	get 'auth/:provider/callback', to: 'sessions#create'
 # get 'auth/failure', to: redirect('/')
 
-  devise_for :admins
-  get '/auth/google_oauth2/callback', :to => 'sessions#create'
+  devise_for :admins, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
+   get '/auth/google_oauth2/callback', :to => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
