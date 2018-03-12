daf0a4260fe697a5e0d897186bc806ddbfa53a0e
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-03-12 12:57:28 +0530

    omniauth-for google added

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,7 @@
 class Admin < ApplicationRecord
 	has_many :articles  
   after_create :send_admin_mail
+
   def send_admin_mail
     UserMailer.send_new_user_message(self).deliver_now
   end
@@ -9,4 +10,26 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = Admin.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    if user
+      return user
+    else
+      registered_user = Admin.where(:email => access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = Admin.create(name: data["name"],
+          provider:access_token.provider,
+          email: data["email"],
+          uid: access_token.uid ,
+          password: Devise.friendly_token[0,20],
+        )
+      end
+    end
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: { registrations: 'admins/registrations' }
 
   root 'students#index'
+
+  devise_for :admins, controllers: { omniauth_callbacks: "omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
