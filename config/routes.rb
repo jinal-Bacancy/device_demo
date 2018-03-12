0ed09a0774c55b81d0d84fc9b040ac65f6c598e7
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-03-12 14:17:45 +0530

    change routes for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,19 @@ Rails.application.routes.draw do
 	end
 	  
   resources :students
-  devise_for :admins, controllers: { registrations: 'admins/registrations' }
+ # devise_for :admins, controllers: { registrations: 'admins/registrations' }
 
   root 'students#index'
 
-  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+#   devise_scope :admin do
+#     get "signup", to: "devise/registrations#new"
+#     get "login", to: "devise/sessions#new"
+#     get "logout", to: "devise/sessions#destroy"
+# 	end
+
+# 	get 'auth/:provider/callback', to: 'sessions#create'
+# get 'auth/failure', to: redirect('/')
+
+  devise_for :admins, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
