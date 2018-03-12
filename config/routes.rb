c1ba0660efcd3d0f77b84f18581a666253947286
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-03-12 15:25:51 +0530

    change in route

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,12 @@ Rails.application.routes.draw do
 # 	get 'auth/:provider/callback', to: 'sessions#create'
 # get 'auth/failure', to: redirect('/')
 
-  devise_for :admins 
-  # :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :admins
 
-   get '/auth/google_oauth2/callback', :to => 'sessions#create'
+
+ #  get '/admin/google_oauth2/callback', :to => 'omniauth_callbacks#google_oauth2'
+
+    #get 'google_login', :controller => 'omniauth_callbacks', :action => 'google_oauth2'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
